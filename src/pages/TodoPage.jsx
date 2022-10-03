05b3ee38966c2172a452a8a55c06350b9ef510a3
emily40830 <emily40830@gmail.com>
2022-10-03 23:51:16 +0800

    feat: add useEffect to get todos from api
    fix: fix import to relative path

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,32 +1,11 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
 import { useState } from 'react';
-
-const dummyTodos = [
-  {
-    title: 'Learn react-router',
-    isDone: true,
-    id: 1,
-  },
-  {
-    title: 'Learn to create custom hooks',
-    isDone: false,
-    id: 2,
-  },
-  {
-    title: 'Learn to use context',
-    isDone: true,
-    id: 3,
-  },
-  {
-    title: 'Learn to implement auth',
-    isDone: false,
-    id: 4,
-  },
-];
+import { useEffect } from 'react';
+import { getTodos } from '../api/todos';
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
-  const [todos, setTodos] = useState(dummyTodos);
+  const [todos, setTodos] = useState([]);
 
   const todoNums = todos.length;
 
@@ -117,6 +96,20 @@ const TodoPage = () => {
   const handleDelete = (id) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
+
+  useEffect(() => {
+    const getTodosAsync = async () => {
+      try {
+        const todos = await getTodos();
+
+        setTodos(todos.map((todo) => ({ ...todo, isEdit: false })));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTodosAsync();
+  }, []);
+
   return (
     <div>
       <Header />
@@ -133,7 +126,7 @@ const TodoPage = () => {
         onChangeMode={handleChangeMode}
         onDelete={handleDelete}
       />
-      <Footer numOfTodos={todoNums}/>
+      <Footer numOfTodos={todoNums} />
     </div>
   );
 };
